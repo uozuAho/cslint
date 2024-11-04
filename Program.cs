6d5042db886336bc33306632d4c4d312f89c5a6d
me <me@me.com>
2024-11-04 15:55:59 +1100

    add ugly code, readme

--- Program.cs
+++ Program.cs
@@ -2,10 +2,40 @@
 {
     public static class Program
     {
-        public static int Main()
+        public static async Task<int> Main()
         {
             Console.WriteLine("yo");
+            var asdf = await DoThing();
+
+            // long line
+            var alskdjflaksjdflkjasjdfljkasdjfalskdfj = 123456789 + 123456789 + 123456789 + 123456789 + 123456789 + 123456789;
+            Console.WriteLine(alskdjflaksjdflkjasjdfljkasdjfalskdfj);
+
+            // missing await
+            var x = DoThing();
             return 0;
         }
+
+        // naming: Async suffix
+        private static async Task<int> DoThing()
+        {
+            await Task.Delay(1);
+            // if block should be in curlies
+            if (1 == 1) Console.Write("yep");
+            return 1;
+
+            // unnecessary whitespace
+
+        }
+
+        // don't do async void
+        private static async void BadAsyncVoidFunc()
+        {
+            await Task.Delay(1);
+        }
+
+        // regions suck
+        #region AASDF
+        #endregion
     }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Example project to test linters
+
+None of the problems in the code are picked up by `dotnet run`
