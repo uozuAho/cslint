188757e63341addc23e9d9795400410d8f26abe7
me <me@me.com>
2025-05-25 16:40:28 +1000

    remove lint resources, WIP script to add

---
+++ add_linting.fsx
@@ -0,0 +1,44 @@
+open System
+open System.IO
+open System.Xml.Linq
+
+let getAllCsprojFiles (directory: string) =
+    Directory.GetFiles(directory, "*.csproj", SearchOption.AllDirectories)
+
+let ensureElement (parent: XElement) (name: string) (value: string) =
+    match parent.Element(XName.Get name) with
+    | null ->
+        let added = XElement(XName.Get name, value)
+        parent.Add(added)
+        added
+    | elem ->
+        elem.Value <- value
+        elem
+
+let updateCsprojFile (filePath: string) =
+    try
+        let doc = XDocument.Load(filePath)
+        let ns = doc.Root.Name.Namespace
+
+        let propertyGroup = ensureElement doc.Root "PropertyGroup" ""
+
+        ensureElement propertyGroup "AnalysisMode" "All" |> ignore
+        ensureElement propertyGroup "CodeAnalysisTreatWarningsAsErrors" "true" |> ignore
+
+        doc.Save(filePath)
+        printfn $"Updated: {filePath}"
+    with ex ->
+        printfn $"Error processing {filePath}: {ex.Message}"
+
+/// Main entry point
+[<EntryPoint>]
+let main argv =
+    if argv.Length <> 1 then
+        printfn "Usage: dotnet fsi script.fsx <directory>"
+        1
+    else
+        let rootDir = argv.[0]
+        let files = getAllCsprojFiles rootDir
+        for file in files do
+            updateCsprojFile file
+        0
