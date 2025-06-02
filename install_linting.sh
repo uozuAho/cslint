aa5e804b6d06c3d41a61757fcfe8e38e03a821b0
me <me@me.com>
2025-06-02 21:03:51 +1000

    fix install linting script

--- install_linting.sh
+++ install_linting.sh
@@ -6,7 +6,9 @@ SLN_ROOT=$1
 
 cp Directory.Build.props.template $SLN_ROOT/Directory.Build.props
 cat .editorconfig.template >> $SLN_ROOT/.editorconfig
-cp lint.sh $SLN_ROOT/
+if [ ! -f "$SLN_ROOT/lint.sh" ]; then
+    cp lint.sh $SLN_ROOT/
+fi
 pushd $SLN_ROOT
 dotnet new tool-manifest
 dotnet tool install csharpier
